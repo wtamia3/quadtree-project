d551c47c8b844d444572a1b24c082f29fe72cfca
wtamia3 <157857380+wtamia3@users.noreply.github.com>
2024-11-30 07:56:08 -0800

    Update Rectangle.java

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,37 +1,23 @@
-package src.main.java;
-
 public class Rectangle {
-    private int x, y, length, width;
+    private double x, y, length, width;
 
-    public Rectangle(int x, int y, int length, int width) {
+    public Rectangle(double x, double y, double length, double width) {
         this.x = x;
         this.y = y;
         this.length = length;
         this.width = width;
     }
 
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public int getLength() { return length; }
-    public int getWidth() { return width; }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 
     @Override
     public String toString() {
-        return "Rectangle(" + "x=" + x + ", y=" + y + ", length=" + length + ", width=" + width + ")";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Rectangle rectangle = (Rectangle) obj;
-        return x == rectangle.x && y == rectangle.y && length == rectangle.length && width == rectangle.width;
-    }
-
-    @Override
-    public int hashCode() {
-        return 31 * (31 * (31 * Integer.hashCode(x) + Integer.hashCode(y)) + Integer.hashCode(length)) + Integer.hashCode(width);
+        return "Rectangle at (" + x + ", " + y + "): " + length + "x" + width;
     }
 }
-
-
