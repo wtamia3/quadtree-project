581ca91aff1672a11e23cc68e74079c1abbc5454
wtamia3 <157857380+wtamia3@users.noreply.github.com>
2024-11-30 07:45:55 -0800

    Update Node.java

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,24 +1,20 @@
-package src.main.java;
-
 public abstract class Node {
-    private int xMin, xMax, yMin, yMax;
+    protected double xMin, yMin, xMax, yMax;
 
-    public Node(int xMin, int xMax, int yMin, int yMax) {
+    public Node(double xMin, double yMin, double xMax, double yMax) {
         this.xMin = xMin;
-        this.xMax = xMax;
         this.yMin = yMin;
+        this.xMax = xMax;
         this.yMax = yMax;
     }
 
-    public abstract void insert(Rectangle rect);
-    public abstract void delete(Rectangle rect);
-    public abstract Rectangle find(int x, int y);
-    public abstract void dump(int level);
+    public abstract void insertRectangle(Rectangle rectangle);
 
-    public int getXMin() { return xMin; }
-    public int getXMax() { return xMax; }
-    public int getYMin() { return yMin; }
-    public int getYMax() { return yMax; }
+    public abstract void deleteRectangle(double x, double y);
+
+    public abstract void findRectangle(double x, double y);
+
+    public abstract void dumpNode(int level);
 }
 
 
