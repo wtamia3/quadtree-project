e28d47d2e6bf4320c0b0ee9f2e137fb4292396e8
wtamia3 <157857380+wtamia3@users.noreply.github.com>
2024-11-30 08:16:59 -0800

    Update NodeTest.java

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -4,17 +4,41 @@ import org.junit.jupiter.api.Test;
 public class NodeTest {
 
     @Test
-    public void testInsertRectangle() {
-        Quadtree quadtree = new Quadtree();
-        quadtree.insertRectangle(10, 10, 5, 5);
-        quadtree.findRectangle(10, 10); // Should print the rectangle
+    public void testNodeCreation() {
+        Node node = new Node(0, 0, 100, 100); // Example Node setup
+        assertNotNull(node);
+    
     }
 
     @Test
-    public void testFindRectangle() {
-        Quadtree quadtree = new Quadtree();
-        quadtree.insertRectangle(10, 10, 5, 5);
-        quadtree.findRectangle(10, 10); // Should find the rectangle
+    public void testLeafNodeCreation() {
+        LeafNode leafNode = new LeafNode(0, 0, 50, 50); // Example LeafNode setup
+        assertNotNull(leafNode);
+    }
+
+    @Test
+    public void testLeafNodeInsertRectangle() {
+        LeafNode leafNode = new LeafNode(0, 0, 50, 50);
+        Rectangle rectangle = new Rectangle(10, 10, 5, 5);
+        assertTrue(leafNode.insertRectangle(rectangle));
+    }
+
+    @Test
+    public void testLeafNodeSplit() {
+        LeafNode leafNode = new LeafNode(0, 0, 50, 50);
+        for (int i = 0; i < 6; i++) {
+            leafNode.insertRectangle(new Rectangle(i * 10, i * 10, 5, 5)); // Add 6 rectangles
+        }
+        assertTrue(leafNode.isSplit());
+        
+    }
+
+    @Test
+    public void testNodeFindRectangle() {
+        Node node = new Node(0, 0, 100, 100);
+        Rectangle rectangle = new Rectangle(10, 10, 5, 5);
+        node.insertRectangle(rectangle);
+        assertNotNull(node.findRectangle(10, 10));
     }
 
 }
